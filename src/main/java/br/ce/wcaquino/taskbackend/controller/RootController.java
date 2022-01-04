3db1cb41c00453d62a2782f7d043977caa9a2edb
INDRA\ehans <ehans@minsait.com>
2022-01-04 12:16:23 -0300

    conexao bd

--- src/main/java/br/ce/wcaquino/taskbackend/controller/RootController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/RootController.java
@@ -1,11 +1,13 @@
 package br.ce.wcaquino.taskbackend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value ="/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RootController {
 
 	
--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import br.ce.wcaquino.taskbackend.repo.TaskRepo;
 import br.ce.wcaquino.taskbackend.utils.DateUtils;
 import br.ce.wcaquino.taskbackend.utils.ValidationException;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value ="/todo")
 public class TaskController {

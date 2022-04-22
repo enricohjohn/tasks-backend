7adf6f99367375c6b0569100136d77ba518bc81e
INDRA\acmelo <acmelo@minsait.com>
2022-04-21 22:51:24 -0300

    Implementação do Teste Unitário referente a DateUtils

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,33 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+	@Test
+	public void testIsEqualOrFutureDate() {
+	}
+	
+	@Test
+	public void deveRetornarTrueParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 1, 1);
+		boolean equalOrFutureDate = DateUtils.isEqualOrFutureDate(date);
+		Assert.assertTrue(equalOrFutureDate);
+	}
+	@Test
+	public void deveRetornarFalseParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2010, 1,1);
+		boolean equalOrFutureDate = DateUtils.isEqualOrFutureDate(date);
+		Assert.assertFalse(equalOrFutureDate);
+	}
+
+	@Test
+	public void deveRetornarTrueParaDataAtual() {
+		LocalDate date = LocalDate.now();
+		boolean equalOrFutureDate = DateUtils.isEqualOrFutureDate(date);
+		Assert.assertTrue(equalOrFutureDate);
+	}
+}
